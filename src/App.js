3217744c603942e8201232a35a764fd9d8e91029
karima <timmermann@gmx.at>
2022-05-26 08:15:29 +0200

    Add type verification and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
 import Header from './Components/header.js';
 import ProductBoard from './Components/products.js';
 import Landing from './Components/landing.js';
 import Contact from "./Components/contact.js";
 import Checkout from "./Components/checkout.js";
-// import { LandingPage, 
-//             // ContactPage,
-// import{
-//          } from "./App.style.js"
-
 
 function App () {
+   const [ quantity, setQuantity ] = useState( new Array(19).fill(0) );
+   const [ ntotal, setNtotal ] = useState(0);
+   console.log("rrrrrrrrrrrrrrrrrrrrrrrrrrr");
+   console.log(quantity);
+
    return (
          <BrowserRouter>
-            <Header/>
+            <Header quantity={ ntotal }/>
             <Routes>
               <Route path="/" element={<Landing/>}/>
-              <Route path="/products" element={<ProductBoard/>}/>
+              <Route path="/products" element={<ProductBoard _quantity={ quantity } _setQuantity={ setQuantity } _setNtotal={ setNtotal }/> }/>
               <Route path="/contact" element={<Contact/>}/>
-              <Route path="/checkout" element={<Checkout/>}/>
+              <Route path="/checkout" element={<Checkout _quantity={ quantity } _setQuantity={ setQuantity } _setNtotal={ setNtotal }/>}/>
             </Routes>
         </BrowserRouter>
    );
--- src/App.style.js
+++ src/App.style.js
@@ -6,7 +6,7 @@ export const LandingPage = styled.div`
     padding:0;
     box-sizing: border-box;
     background: rgb(225,239,255);
-    background: linear-gradient(342deg, rgba(225,239,255,1) 3%, rgba(206,192,182,1) 97%); 
+    background: linear-gradient(320deg, rgba(225,239,255,1) 3%, rgba(206,192,182,1) 97%); 
 `
 
 export const ContactPage = styled.div`
