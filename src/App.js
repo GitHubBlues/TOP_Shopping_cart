0f12660174c4b846c851b5a1e62ffb1afcd525bf
karima <timmermann@gmx.at>
2022-11-22 13:42:13 +0100

    Remove not needed code

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import Checkout from "./Components/checkout.js";
 function App () {
    const [ quantity, setQuantity ] = useState( new Array(19).fill(0) );
    const [ ntotal, setNtotal ] = useState(0);
-   console.log("rrrrrrrrrrrrrrrrrrrrrrrrrrr");
-   console.log(quantity);
 
    return (
          <BrowserRouter>
